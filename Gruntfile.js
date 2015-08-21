6ec8761e750411e6f28852d8c79a224efab4a416
Shakup <sebastien.decamme@gmail.com>
2015-08-21 17:19:14 +0200

    Setup lea.evolution.js

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		banner: '/*\n' +
+			' * <%= pkg.name %> - version <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			' * <%= pkg.description %>\n' +
+			' * Author: <%= pkg.author %>\n' +
+			' * Homepage: <%= pkg.homepage %>\n' +
+			' */',
+		
+		usebanner: {
+			dist: {
+				options: {
+					position: 'top',
+					banner: '<%= banner %>'
+				},
+				files: {
+					src: ['dist/lea.evolution.js']
+				}
+			}
+		},
+
+		replace: {
+			dist: {
+				src: ['dist/lea.evolution.js'],
+				overwrite: true,
+				replacements: [
+					{
+						from: '{{version}}',
+						to: '<%= pkg.version %>'
+					},
+					{
+						from: '{{homepage}}',
+						to: '<%= pkg.homepage %>'
+					}
+				]
+			}
+		},
+
+		uglify: {
+			options: {
+				mangle: true,
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'dist/lea.evolution.js': ['src/lea.evolution.js']
+				}
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['src/*.js'],
+				tasks: ['uglify','usebanner','replace']
+			}
+		},
+
+		notify_hooks: {
+			options: {
+				enabled: true,
+				max_jshint_notifications: 2,
+				title: "Lea.evolution.js",
+				success: true,
+				duration: 2
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-banner');
+	grunt.loadNpmTasks('grunt-text-replace');
+	grunt.loadNpmTasks('grunt-notify');
+
+	grunt.task.run('notify_hooks');
+	
+	grunt.registerTask('default', ['uglify','usebanner','replace']);
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# lea.evolution.js
+> A JavaScript framework to create beautiful applications based on lea.js
+
+**Work in progress**
+
+## Author
+
+* Email: [sebastien.decamme@gmail.com](mailto:sebastien.decamme@gmail.com)
+* Twitter: [@sdecamme](https://twitter.com/sdecamme)
+* Website: [http://sebastiendecamme.fr](http://sebastiendecamme.fr)
+
+
+## Copyright and license
+
+Copyright 2014-2015 Sébastien Decamme. Released under [MIT](http://opensource.org/licenses/MIT).
